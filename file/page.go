80fa671abb9a3f55b5118dd0869f675c1c3f2ee0
nfphys <fumiya-nakamura@c-fo.com>
2025-04-12 00:55:20 +0900

    定数INT_BYTESを追加

--- file/page.go
+++ file/page.go
@@ -4,6 +4,10 @@ import (
 	"encoding/binary"
 )
 
+const (
+	INT_BYTES = 4
+)
+
 type Page struct {
 	b []byte
 }
@@ -21,21 +25,21 @@ func NewPageFromBytes(b []byte) *Page {
 }
 
 func (p *Page) GetInt(offset int) int {
-	return int(binary.LittleEndian.Uint32(p.b[offset:offset+4]))
+	return int(binary.LittleEndian.Uint32(p.b[offset:offset+INT_BYTES]))
 }
 
 func (p *Page) SetInt(offset int, n int) {
-	binary.LittleEndian.PutUint32(p.b[offset:offset+4], uint32(n))
+	binary.LittleEndian.PutUint32(p.b[offset:offset+INT_BYTES], uint32(n))
 }
 
 func (p *Page) GetBytes(offset int) []byte {
 	length := p.GetInt(offset) // byte列は、先頭4バイトに長さが格納されている
-	return p.b[offset+4 : offset+4+length]
+	return p.b[offset+INT_BYTES : offset+INT_BYTES+length]
 }
 
 func (p *Page) SetBytes(offset int, b []byte) {
 	p.SetInt(offset, len(b)) // byte列は、先頭4バイトに長さを格納する
-	copy(p.b[offset+4:], b)
+	copy(p.b[offset+INT_BYTES:], b)
 }
 
 func (p *Page) GetString(offset int) string {
